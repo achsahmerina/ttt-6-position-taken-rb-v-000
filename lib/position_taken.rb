99821cee849b26cd3af92873f0f221260da4946b
Achsah Abraham <achsahmp@gmail.com>
2018-09-24 17:32:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,3 +3,9 @@ def position_taken(board, index)
   (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
     false : true
 end
+
+puts position_taken?(ttt_board, 0)
+puts position_taken?(ttt_board, 1)
+puts position_taken?(ttt_board, 2)
+puts position_taken?(ttt_board, 3)
+puts position_taken?(ttt_board, 4)

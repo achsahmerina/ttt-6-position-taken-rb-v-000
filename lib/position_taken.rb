8ee1a1a201f169d33a4224e90c29d8ffd6d1f7d2
Achsah Abraham <achsahmp@gmail.com>
2018-09-24 17:32:58 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,10 +2,9 @@ ttt_board = [" "," "," "," ","X"," "," "," "," "]
 def position_taken(board, index)
   (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
     false : true
+    puts position_taken?(ttt_board, 0)
+    puts position_taken?(ttt_board, 1)
+    puts position_taken?(ttt_board, 2)
+    puts position_taken?(ttt_board, 3)
+    puts position_taken?(ttt_board, 4)
 end
-
-puts position_taken?(ttt_board, 0)
-puts position_taken?(ttt_board, 1)
-puts position_taken?(ttt_board, 2)
-puts position_taken?(ttt_board, 3)
-puts position_taken?(ttt_board, 4)
